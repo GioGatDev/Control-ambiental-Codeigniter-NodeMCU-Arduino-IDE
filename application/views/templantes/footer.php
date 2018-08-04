78b6c87b31d1c9aee0226a6d99da3b7a917f60fc
GiovanniGatDev <giovannigatfut@gmail.com>
2018-08-04 15:55:09 -0500

    Agregando footer

---
+++ application/views/templantes/footer.php
@@ -0,0 +1,83 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+  <div class="footer">
+    <div>
+      <strong>Copyright</strong> IA 2017 &copy;
+    </div>
+  </div>
+
+
+    <!-- PROYECTOS -->
+    <script src="<?php echo base_url();?>js/domo/empleado.js"></script>
+
+    <!-- LOGIN -->
+    <script src="<?php echo base_url();?>js/domo/login.js"></script>
+
+    <!-- CONTROL DE PUERTAS Y LUCES -->
+    <script src="<?php echo base_url();?>js/domo/control_botones.js"></script>
+
+    <!-- Mainly scripts -->
+    <script src="<?php echo base_url(); ?>js/jquery-2.1.1.js"></script>
+    <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="<?php echo base_url(); ?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Flot -->
+    <script src="<?php echo base_url(); ?>js/plugins/flot/jquery.flot.js"></script>
+    <script src="<?php echo base_url(); ?>js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+    <script src="<?php echo base_url(); ?>js/plugins/flot/jquery.flot.spline.js"></script>
+    <script src="<?php echo base_url(); ?>js/plugins/flot/jquery.flot.resize.js"></script>
+    <script src="<?php echo base_url(); ?>js/plugins/flot/jquery.flot.pie.js"></script>
+
+    <!-- Peity -->
+    <script src="<?php echo base_url(); ?>js/plugins/peity/jquery.peity.min.js"></script>
+    <script src="<?php echo base_url(); ?>js/demo/peity-demo.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="<?php echo base_url(); ?>js/inspinia.js"></script>
+    <script src="<?php echo base_url(); ?>js/plugins/pace/pace.min.js"></script>
+
+    <!-- jQuery UI -->
+    <script src="<?php echo base_url(); ?>js/plugins/jquery-ui/jquery-ui.min.js"></script>
+
+    <!-- GITTER -->
+    <script src="<?php echo base_url(); ?>js/plugins/gritter/jquery.gritter.min.js"></script>
+
+    <!-- Sparkline -->
+    <script src="<?php echo base_url(); ?>js/plugins/sparkline/jquery.sparkline.min.js"></script>
+
+    <!-- Sparkline demo data  -->
+    <script src="<?php echo base_url(); ?>js/demo/sparkline-demo.js"></script>
+
+    <!-- Chosen -->
+    <script src="<?php echo base_url(); ?>js/plugins/chosen/chosen.jquery.js"></script>
+
+
+    <!-- ChartJS-->
+    <script src="<?php echo base_url(); ?>js/plugins/chartJs/Chart.min.js"></script>
+
+    <!-- Toastr -->
+    <script src="<?php echo base_url(); ?>js/plugins/toastr/toastr.min.js"></script>
+    <!-- Steps -->
+    <script src="<?php echo base_url(); ?>js/plugins/staps/jquery.steps.min.js"></script>
+
+    <!-- Tags Input -->
+    <script src="<?php echo base_url(); ?>js/plugins/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
+
+    <!-- Jquery Validate -->
+    <script src="<?php echo base_url(); ?>js/plugins/validate/jquery.validate.min.js"></script>
+    <!-- Typehead -->
+    <script src="<?php echo base_url(); ?>js/plugins/typehead/bootstrap3-typeahead.min.js"></script>
+    <!-- Select2 -->
+    <script src="<?php echo base_url(); ?>js/plugins/select2/select2.full.min.js"></script>
+    <!-- Input Mask-->
+    <script src="<?php echo base_url(); ?>js/plugins/jasny/jasny-bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>js/plugins/switchery/switchery.js"> </script>
+
+    <!-- ImagePreview -->
+    <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.uploadPreview.min.js"></script>
+
+    <!-- Sweet alert -->
+    <script src="<?php echo base_url(); ?>js/plugins/sweetalert/sweetalert.min.js"></script>
+
+  </body>
+</html>
