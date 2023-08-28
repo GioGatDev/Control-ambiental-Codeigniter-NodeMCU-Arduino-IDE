84989d9f925495c7b6cc1c9cfc415c0749a5d4ec
GioDev <giovannigatfut@gmail.com>
2023-08-28 15:52:59 -0600

    update JQuery

--- application/views/templantes/footer.php
+++ application/views/templantes/footer.php
@@ -6,7 +6,7 @@
   </div>
 
     <!-- Mainly scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" crossorigin="anonymous" async></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" crossorigin="anonymous" async></script>
     <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
     <script src="<?php echo base_url(); ?>js/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="<?php echo base_url(); ?>js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
